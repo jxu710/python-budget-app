f6a922e9efb6f6669b1d346b527ff684fec80f72
jxu710 <101676244+jxu710@users.noreply.github.com>
2023-11-21 00:37:11 +0000

    Set up structure of needed functions

--- budget.py
+++ budget.py
@@ -1,7 +1,27 @@
 class Category:
 
+  def __init__(self,category):
+    self.ledger = []
+    self.category = category
 
+  def deposit(self, amount, description=""):
+    self.ledger.append({"amount": amount, "description": description})
 
+  def withdraw(self, amount, description=""):
+    self.ledger.append({"amount": -amount, "description": description})
+
+  def check_funds(self, amount):
+    sum_of_ledge = 0
+    for item in self.ledger:
+      sum_of_ledge += item["amount"]
+    return sum_of_ledge < amount
+
+  def get_balance(self):
+    pass
+
+  def transfer(self):
+    pass
 
 
 def create_spend_chart(categories):
+  print(categories)
